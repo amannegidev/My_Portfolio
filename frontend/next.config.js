585f04316394c22935c0361c3c323c6a9d8f0afb
amannegidev <amannegi2314@gmail.com>
2025-12-17 16:06:31 +0530

    new

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -27,6 +29,10 @@ const nextConfig = {
   env: {
     BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:5000',
   },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname)
+    return config
+  }
 }
 
 module.exports = nextConfig
