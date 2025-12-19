6edce4cf2a44acdb535253d82a59eb7e7038deaf
amannegidev <amannegi2314@gmail.com>
2025-12-19 22:09:53 +0530

    pr-6

--- frontend/components/ContactSection.tsx
+++ frontend/components/ContactSection.tsx
@@ -17,11 +17,11 @@ export default function ContactSection({
   return (
     <section className="pt-6 lg:py-20">
       <div className="container mx-auto px-4 text-start md:text-center ">
-        <div className="contact-box rounded-lg bg-black py-12 px-4 text-white ">
+        <div className="contact-box rounded-lg bg-black py-12 pr-6 text-white ">
           <h2 className="text-xl lg:text-5xl  mb-6 text-white leading-relaxed">
             {title}
           </h2>
-          <p className="text-lg mb-8 mx-auto  display-none md-display-block">
+          <p className="text-sm md:text-lg mb-8 mx-auto  display-none md-display-block">
             {description}
           </p>
           <Link 
